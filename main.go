1f7dcb29a8c60abd3a5c25579812a1edf35ef25a
German Feskov <g.feskov@yandex.ru>
2025-04-17 18:06:10 +0300

    release 3

--- main.go
+++ main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
-	"os"
-	"net/http"
+	"context"
 	"fmt"
 	"log"
+	"net/http"
+	"os"
+	"time"
 
 	"github.com/redis/go-redis/v9"
 )
 
+var rdb *redis.Client
+
 func main() {
+	// HTTP-заглушка для Timeweb
 	go func() {
 		port := os.Getenv("PORT")
 		if port == "" {
@@ -18,15 +23,26 @@ func main() {
 		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintln(w, "Telegram bot is alive!")
 		})
+		log.Println("HTTP server running on port:", port)
 		if err := http.ListenAndServe(":"+port, nil); err != nil {
 			log.Fatalf("HTTP server error: %v", err)
 		}
 	}()
 
+	// Инициализация Redis
 	rdb = redis.NewClient(&redis.Options{
-		Addr:     os.Getenv("REDIS_ADDR"),
+		Addr:     os.Getenv("REDIS_ADDR"), // "redis:6379" через docker-compose
 		Password: "",
 		DB:       0,
 	})
+	if _, err := rdb.Ping(context.Background()).Result(); err != nil {
+		log.Fatalf("Redis connection error: %v", err)
+	}
+
+	// Запуск Telegram-бота
 	InitTelegramAPI()
+
+	// 🚨 Чтобы main не завершался
+	select {} // блокирует навсегда
 }
+
