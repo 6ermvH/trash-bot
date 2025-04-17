0539fb061c75beb1b485cd81448459bc155e41bf
German Feskov <g.feskov@yandex.ru>
2025-04-17 18:01:24 +0300

    release 2

--- main.go
+++ main.go
@@ -15,9 +15,16 @@ func main() {
 	})
 	InitTelegramAPI()
 
-	port := os.Getenv("PORT")
-	if port == "" {
+	go func() {
+		port := os.Getenv("PORT")
+		if port == "" {
 			port = "8080"
-	}
-	http.ListenAndServe(":"+port, nil)
+		}
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintln(w, "Telegram bot is alive!")
+		})
+		if err := http.ListenAndServe(":"+port, nil); err != nil {
+			log.Fatalf("HTTP server error: %v", err)
+		}
+	}()
 }
