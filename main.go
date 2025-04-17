6d3d4aaf03b21ad6896ce4f623136dc0de5ef411
German Feskov <g.feskov@yandex.ru>
2025-04-17 18:16:00 +0300

    release 5

--- main.go
+++ main.go
@@ -1,10 +1,6 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"log"
-	"net/http"
 	"os"
 
 	"github.com/redis/go-redis/v9"
@@ -16,22 +12,22 @@ func main() {
 		Password: "",
 		DB:       0,
 	})
-	if _, err := rdb.Ping(context.Background()).Result(); err != nil {
-		log.Fatalf("Redis connection error: %v", err)
-	}
+	go InitTelegramAPI()
 
-	InitTelegramAPI()
+	startHTTPHealthServer()
+}
 
+func startHTTPHealthServer() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Telegram bot is alive!")
+		fmt.Fprintln(w, "Bot is alive")
 	})
-	log.Printf("🌐 HTTP server running on port %s\n", port)
+
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		log.Fatalf("HTTP server error: %v", err)
+		panic(err)
 	}
 }
-
