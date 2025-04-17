44bb9b5109a342bc30044c8ef2fdf4c8ac67d637
German Feskov <g.feskov@yandex.ru>
2025-04-17 18:09:02 +0300

    release 3

--- main.go
+++ main.go
@@ -6,32 +6,13 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"time"
 
 	"github.com/redis/go-redis/v9"
 )
 
-var rdb *redis.Client
-
 func main() {
-	// HTTP-заглушка для Timeweb
-	go func() {
-		port := os.Getenv("PORT")
-		if port == "" {
-			port = "8080"
-		}
-		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintln(w, "Telegram bot is alive!")
-		})
-		log.Println("HTTP server running on port:", port)
-		if err := http.ListenAndServe(":"+port, nil); err != nil {
-			log.Fatalf("HTTP server error: %v", err)
-		}
-	}()
-
-	// Инициализация Redis
 	rdb = redis.NewClient(&redis.Options{
-		Addr:     os.Getenv("REDIS_ADDR"), // "redis:6379" через docker-compose
+		Addr:     os.Getenv("REDIS_ADDR"),
 		Password: "",
 		DB:       0,
 	})
@@ -39,10 +20,18 @@ func main() {
 		log.Fatalf("Redis connection error: %v", err)
 	}
 
-	// Запуск Telegram-бота
 	InitTelegramAPI()
 
-	// 🚨 Чтобы main не завершался
-	select {} // блокирует навсегда
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Telegram bot is alive!")
+	})
+	log.Printf("🌐 HTTP server running on port %s\n", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatalf("HTTP server error: %v", err)
+	}
 }
 
