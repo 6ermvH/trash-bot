10f3425d49a9aa24fe403e0f1ea770cb8d0e75d1
German Feskov <g.feskov@yandex.ru>
2025-04-18 15:51:44 +0300

    24

--- main.go
+++ main.go
@@ -1,29 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
 	"os"
 
 	"github.com/redis/go-redis/v9"
 )
-
-func server() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		fmt.Fprintln(w, "✅ Telegram bot is alive")
-	})
-
-	log.Printf("🌐 Listening on port %s", port)
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		log.Printf("⚠️ HTTP server error: %v", err)
-	}
-}
 
 func main() {
 	rdb = redis.NewClient(&redis.Options{
@@ -32,8 +13,5 @@ func main() {
 		DB:       0,
 	})
 
-	go InitTelegramAPI()
-	go server()
-
-	select {}
+	InitTelegramAPI()
 }
