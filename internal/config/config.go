2fd14c4b228b87d6c4c10043e74005b8e19c1423
German Feskov <g.feskov@yandex.ru>
2026-01-20 01:47:18 +0300

    Fix YAML struct tags

--- internal/config/config.go
+++ internal/config/config.go
@@ -9,30 +9,30 @@ import (
 
 // Config is type configuration of service.
 type Config struct {
-	Telegram TelegramCfg `yaml: telegram`
-	Server   ServerCfg   `yaml: server`
-	Database DatabaseCfg `yaml: database`
+	Telegram TelegramCfg `yaml:"telegram"`
+	Server   ServerCfg   `yaml:"server"`
+	Database DatabaseCfg `yaml:"database"`
 }
 
 // DatabaseCfg is type database configuration.
 type DatabaseCfg struct {
-	Type string `yaml: type` // "memory" or "sqlite"
-	Path string `yaml: path` // path to sqlite file
+	Type string `yaml:"type"` // "memory" or "sqlite"
+	Path string `yaml:"path"` // path to sqlite file
 }
 
 // TelegramCfg is type telegram configuration.
 type TelegramCfg struct {
-	BotKey string `yaml: botkey`
+	BotKey string `yaml:"botkey"`
 }
 
 // ServerCfg is type server configuration.
 type ServerCfg struct {
-	Enabled       bool   `yaml: enabled`
-	Addr          string `yaml: addr`
-	Port          string `yaml: port`
-	AdminLogin    string `yaml: adminlogin`
-	AdminPassword string `yaml: adminpassword`
-	JWTSecret     string `yaml: jwtsecret`
+	Enabled       bool   `yaml:"enabled"`
+	Addr          string `yaml:"addr"`
+	Port          string `yaml:"port"`
+	AdminLogin    string `yaml:"adminlogin"`
+	AdminPassword string `yaml:"adminpassword"`
+	JWTSecret     string `yaml:"jwtsecret"`
 }
 
 // New create empty Config.
